1575c4991655453e1d06ec5978756bcf0c7c1a60
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-12-22 14:25:38 +0500

    second commit

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/card";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Rehan Hussain</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam tempora
+        expedita maiores voluptatibus amet voluptates consequatur, quis,
+        temporibus nemo aut aliquid. Atque voluptatem sit eveniet quos quis
+        voluptas porro ullam!
+      </p>
+    </div>
+  );
+};
+
+export default Card;
