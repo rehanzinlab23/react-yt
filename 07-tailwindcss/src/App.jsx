2116001767402bb9cf80aef07439574fdbb3140d
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-12-24 16:29:51 +0500

    learning tailwind

---
+++ 07-tailwindcss/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="h-full w-full bg-gray-900">
+      <div className="h-10 w-10 bg-amber-900 rounded-3xl">Box</div>
+    </div>
+  );
+};
+
+export default App;
